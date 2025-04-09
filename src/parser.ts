4ed5ff89c4b081e8aa68a0cd8425930c486c7dcb
Nathaniel <314009+nathanielks@users.noreply.github.com>
2025-04-09 19:35:52 -0400

    remove unused files

--- src/parser.ts
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ts-node
-
-import * as csv from "@fast-csv/parse";
-
-csv
-	.parseStream(process.stdin)
-	.on("error", (error) => console.error(error))
-	.on("data", (row) => console.log(row))
-	.on("end", (rowCount: number) => console.log(`Parsed ${rowCount} rows`));
